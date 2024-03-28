27b98e01322e0c212fb80dc6f79d28fdf1a2a48d
Jeremy <jeremy.isenburg@gmail.com>
2024-03-28 02:26:51 -0500

    Bug for form field refreshing pdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,54 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      title: 'Title',
+      home: const ComboBoxDemo(),
+      routes: {
+        TestScreen.routeName: (ctx) => const TestScreen(),
+      },
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ComboBoxDemo extends StatefulWidget {
+  const ComboBoxDemo({super.key});
 
   @override
+  State<ComboBoxDemo> createState() => _ComboBoxDemoState();
+}
+
+class _ComboBoxDemoState extends State<ComboBoxDemo> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.of(context)
+              .pushNamed(TestScreen.routeName, arguments: "some arg")
+              .then((value) => setState(() {})),
+          child: const Text('press me'),
+        ),
       ),
-      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
+class TestScreen extends StatelessWidget {
+  static const routeName = '/route';
 
-  MyHomePage({super.key});
+  const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            SizedBox(
-              height: 500,
-              child: SfPdfViewer.asset('assets/form_template.pdf'),
-            ),
-            Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(50.0),
-                child: Column(
-                  children: <Widget>[TextFormField(initialValue: "hi")],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    print('building new screen!');
+    // final args = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(body: SfPdfViewer.asset('assets/radio.pdf'));
   }
 }
